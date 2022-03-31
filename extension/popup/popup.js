49a716a7546fe56ec49ae81658cf3b6dc722e25b
Basti Buck <mail@bastibuck.de>
2022-03-31 20:59:08 +0200

    add subject inputs

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -8,11 +8,21 @@ const email = document.querySelector("#email");
 const date = document.querySelector("#meeting-date");
 const time = document.querySelector("#meeting-time");
 
+const subject1Input = document.querySelector("#subject1");
 const vorlage1TextArea = document.querySelector("#vorlage1");
+const subject2Input = document.querySelector("#subject2");
 const vorlage2TextArea = document.querySelector("#vorlage2");
+
+chrome.storage.local.get(["subject1"], function ({ subject1 }) {
+  subject1Input.value = subject1 ?? "";
+});
 
 chrome.storage.local.get(["vorlage1"], function ({ vorlage1 }) {
   vorlage1TextArea.value = vorlage1 ?? "";
+});
+
+chrome.storage.local.get(["subject2"], function ({ subject2 }) {
+  subject2Input.value = subject2 ?? "";
 });
 
 chrome.storage.local.get(["vorlage2"], function ({ vorlage2 }) {
@@ -22,8 +32,16 @@ chrome.storage.local.get(["vorlage2"], function ({ vorlage2 }) {
 triggerVorlage1Btn.addEventListener("click", () => messageContentScript(1));
 triggerVorlage2Btn.addEventListener("click", () => messageContentScript(2));
 
+subject1Input.addEventListener("input", function (event) {
+  chrome.storage.local.set({ subject1: event.target.value });
+});
+
 vorlage1TextArea.addEventListener("input", function (event) {
   chrome.storage.local.set({ vorlage1: event.target.value });
+});
+
+subject2Input.addEventListener("input", function (event) {
+  chrome.storage.local.set({ subject2: event.target.value });
 });
 
 vorlage2TextArea.addEventListener("input", function (event) {
