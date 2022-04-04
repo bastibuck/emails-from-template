bb662286e138a633213cb8f1dd84fe25f623b482
Basti Buck <mail@bastibuck.de>
2022-04-04 22:18:32 +0200

    fill to and subject fields

--- extension/content/content.js
+++ extension/content/content.js
@@ -1,11 +1,31 @@
 chrome.runtime.onMessage.addListener(async (payload, sender, sendResponse) => {
+  // fill to field
+  const input = document.createElement("input");
+  document.body.appendChild(input);
+  input.value = payload.to;
+  input.focus();
+  input.select();
+  document.execCommand("copy");
+
   const toField = document.querySelector("[aria-label='An']");
-  toField.value = payload.to;
+  toField.focus();
+  document.execCommand("paste");
+
+  // fill subject field
+  input.value = payload.subject;
+  input.focus();
+  input.select();
+  document.execCommand("copy");
 
   const subjectField = document.querySelector(
     "[placeholder='Betreff hinzufügen']"
   );
-  subjectField.value = payload.subject;
+  subjectField.focus();
+  document.execCommand("paste");
+  subjectField.blur();
+
+  // remove fake input
+  input.remove();
 
   const bodyField = document.querySelector("[aria-label='Nachrichtentext']");
   bodyField.innerHTML = payload.body;
